c7be1188cf8b5d5cea251020ac37b43985cb37d9
KayakOnWheels <services.62jqb2nu@outlook.com>
2024-07-25 18:09:53 +0200

    First commit

---
+++ src/main/java/com/gear2go_frontend/Gear2GoFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.gear2go_frontend.gear2go_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gear2GoFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gear2GoFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gear2go_frontend/Gear2GoFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gear2go_frontend.gear2go_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gear2GoFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
