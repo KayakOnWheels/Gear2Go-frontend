705c67ade822c4fe1ccf380b9b0c4aca18914c9c
KayakOnWheels <services.62jqb2nu@outlook.com>
2024-07-30 12:26:38 +0200

    Added method resposible for sending request with jwt

--- src/main/java/com/gear2go_frontend/dto/CreateProductRequest.java
+++
@@ -1,6 +0,0 @@
-package com.gear2go_frontend.dto;
-
-import java.math.BigDecimal;
-
-public record CreateProductRequest(String name, Float weight, BigDecimal price, Integer stock) {
-}
--- src/main/java/com/gear2go_frontend/service/UriService.java
+++ src/main/java/com/gear2go_frontend/service/UriService.java
@@ -1,13 +1,24 @@
 package com.gear2go_frontend.service;
 
+import com.vaadin.flow.component.page.WebStorage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
 
 @Service
+@RequiredArgsConstructor
 public class UriService {
+
+    private final RestTemplate restTemplate;
 
     public URI buildUri(String url, MultiValueMap<String, String> parameters) {
 
@@ -16,5 +27,34 @@ public class UriService {
                 .build()
                 .encode()
                 .toUri();
+    }
+
+    public URI buildUri(String url) {
+
+        return UriComponentsBuilder.fromHttpUrl(url)
+                .build()
+                .encode()
+                .toUri();
+    }
+
+    public <T> void getList(String endpoint, ParameterizedTypeReference<List<T>> responseType, Consumer<List<T>> onSuccess, Consumer<Throwable> onError) {
+        URI uri = buildUri(endpoint);
+
+        WebStorage.getItem(WebStorage.Storage.LOCAL_STORAGE, "jwtToken", token -> {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            headers.set("Authorization", "Bearer " + token);
+
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            try {
+                ResponseEntity<List<T>> response = restTemplate.exchange(uri, HttpMethod.GET, entity, responseType);
+                List<T> body = response.getBody();
+                List<T> list = body != null ? body : Collections.emptyList();
+                onSuccess.accept(list);
+            } catch (Exception e) {
+                onError.accept(e);
+            }
+        });
     }
 }
