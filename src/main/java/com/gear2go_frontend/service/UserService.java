0607a01dcb88a01fe11ffbfd372a9010b52fecd0
KayakOnWheels <services.62jqb2nu@outlook.com>
2024-07-30 12:55:50 +0200

    Added UserService and UserCrudPanel

---
+++ src/main/java/com/gear2go_frontend/service/UserService.java
@@ -0,0 +1,71 @@
+package com.gear2go_frontend.service;
+
+import com.gear2go_frontend.customInterface.TokenCallback;
+import com.gear2go_frontend.domain.AuthenticationRequest;
+import com.gear2go_frontend.domain.AuthenticationResponse;
+import com.gear2go_frontend.domain.Product;
+import com.gear2go_frontend.domain.User;
+import com.gear2go_frontend.properties.Gear2GoServerProperties;
+import com.vaadin.flow.component.page.WebStorage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final RestTemplate restTemplate;
+    private final UriService uriService;
+    private final Gear2GoServerProperties gear2GoServerProperties;
+
+
+    public void getUserList(Consumer<List<User>> onSuccess, Consumer<Throwable> onError) {
+        uriService.getList(gear2GoServerProperties.getUser(), new ParameterizedTypeReference<List<User>>() {},
+                onSuccess,
+                onError
+        );
+    }
+
+
+    public List<User> findUsersByName(String lastName) {
+        URI uri = uriService.buildUri(gear2GoServerProperties.getEndpoint() + "user/" + lastName);
+        User[] userArray = restTemplate.getForObject(uri, User[].class);
+
+        return userArray != null ? Arrays.asList(userArray) : Collections.emptyList();
+    }
+
+    public void updateUser(User user) {
+        URI uri = uriService.buildUri(gear2GoServerProperties.getEndpoint() + "user");
+        restTemplate.put(uri, user);
+    }
+
+    public void addUser(User user) {
+        URI uri = uriService.buildUri(gear2GoServerProperties.getEndpoint() + "user");
+        restTemplate.postForEntity(uri, user, Void.class);
+    }
+
+    public void deleteUser(Long id) {
+        URI uri = uriService.buildUri(gear2GoServerProperties.getEndpoint() + "user/" + id);
+        restTemplate.delete(uri);
+    }
+
+    public void getAuthenticationToken(AuthenticationRequest authenticationRequest) {
+        URI uri = uriService.buildUri(gear2GoServerProperties.getEndpoint() + "auth/authenticate");
+        String token = restTemplate.postForObject(uri, authenticationRequest, AuthenticationResponse.class).token();
+        WebStorage.setItem(WebStorage.Storage.LOCAL_STORAGE, "jwtToken", token);
+    }
+
+}
--- src/main/java/com/gear2go_frontend/view/UserCrudPanel.java
+++ src/main/java/com/gear2go_frontend/view/UserCrudPanel.java
@@ -1,4 +1,73 @@
 package com.gear2go_frontend.view;
 
-public class UserCrudPanel {
+import com.gear2go_frontend.domain.User;
+import com.gear2go_frontend.properties.Gear2GoServerProperties;
+import com.gear2go_frontend.service.UriService;
+import com.gear2go_frontend.service.UserService;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+@Route(value = "user-crud", layout = Layout.class)
+public class UserCrudPanel extends VerticalLayout {
+
+    private UserService userService;
+    private Grid<User> grid = new Grid<>(User.class);
+    private TextField filter = new TextField();
+    private UserForm userForm;
+
+    @Autowired
+    public UserCrudPanel(UserService userService) {
+        this.userService = userService;
+        this.userForm = new UserForm(userService, this);
+
+        filter.setPlaceholder("Filter by title");
+        filter.setClearButtonVisible(true);
+        filter.setValueChangeMode(ValueChangeMode.EAGER);
+        filter.addValueChangeListener(e -> update());
+
+        grid.setColumns("id", "firstName", "lastName", "mail", "accessLevel");
+        HorizontalLayout mainContent = new HorizontalLayout(grid, userForm);
+        mainContent.setSizeFull();
+        grid.setSizeFull();
+        userForm.setUserFormVisibility(null);
+
+        grid.asSingleSelect().addValueChangeListener(e -> {
+            userForm.setUserFormVisibility(grid.asSingleSelect().getValue());
+        });
+
+        HorizontalLayout toolbar = new HorizontalLayout(filter);
+
+        add(toolbar, mainContent);
+        setSizeFull();
+        refresh();
+    }
+
+    public void refresh() {
+        userService.getUserList(
+                userList -> {
+                    grid.setItems(userList);
+                },
+                exception -> {
+                    Notification notification = Notification
+                            .show("Something went wrong...");
+                    notification.setPosition(Notification.Position.MIDDLE);
+                    notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                });
+    }
+
+    private void update() {
+        grid.setItems(userService.findUsersByName(filter.getValue()));
+    }
+
 }
