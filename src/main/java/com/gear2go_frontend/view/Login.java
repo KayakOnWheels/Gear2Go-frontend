def91ef7ef319444822e4964d73865cb2e32dc48
KayakOnWheels <services.62jqb2nu@outlook.com>
2024-07-30 14:55:10 +0200

    Added login handling

--- src/main/java/com/gear2go_frontend/view/Login.java
+++ src/main/java/com/gear2go_frontend/view/Login.java
@@ -1,20 +1,30 @@
 package com.gear2go_frontend.view;
 
+import com.gear2go_frontend.domain.AuthenticationRequest;
+import com.gear2go_frontend.service.UserService;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.router.Route;
+import lombok.RequiredArgsConstructor;
 
-@Route("login")
+@Route(value = "login")
 public class Login extends Div {
 
-    public Login() {
+    private final UserService userService;
+
+    public Login(UserService userService) {
+        this.userService = userService;
         LoginOverlay loginOverlay = new LoginOverlay();
         loginOverlay.setError(true);
         add(loginOverlay);
         loginOverlay.setOpened(true);
-        // Prevent the example from stealing focus when browsing the
-        // documentation
-        loginOverlay.getElement().setAttribute("no-autofocus", "");
-    }
 
+        loginOverlay.addLoginListener(e -> {
+            userService.getAuthenticationToken(new AuthenticationRequest(
+                    e.getUsername(),
+                    e.getPassword()
+            ));
+            getUI().ifPresent(ui -> ui.navigate(ProductView.class));
+        });
+    }
 }
